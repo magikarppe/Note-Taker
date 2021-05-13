7222657ccd5708b618eba70f590ee24a51f63079
Thalia De Mattos <tgdmattos@gmail.com>
2021-05-13 14:58:58 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+// require express
+const express = require("express");
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => console.log(`Listen on PORT: http://Localhost:${PORT}`));
